a06defbe0bc81df66fcfedab9e5313ad8bacb2c9
Songkun udom <songkun.u@bluebik.com>
2024-05-11 11:51:13 +0700

    ProductHomePage

--- apps/core/lib/main.dart
+++ apps/core/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:product/main.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ProductHomePage(title: 'Core App'),
     );
   }
 }
@@ -106,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Core',
             ),
             Text(
               '$_counter',
